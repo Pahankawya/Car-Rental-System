2f16b24d988713b95ac23ff6ad23692d8e5c36c1
Pahankawya <pahant24@gmail.com>
2023-11-01 18:02:44 +0530

    back end entities and controller

--- 01_Front_End/assests/scripts/AdminDashboard.js
+++ 01_Front_End/assests/scripts/AdminDashboard.js
@@ -28,55 +28,89 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
 
-    document.getElementById("DashboardForm").addEventListener('click', function () {
+    document.getElementById("DashboardPorm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'inline-block';
     document.getElementById("CarF").style.display = 'none';
     document.getElementById("CustomerF").style.display = 'none';
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
 
-    document.getElementById("CarForm").addEventListener('click', function () {
+    document.getElementById("CarPorm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
     document.getElementById("CarF").style.display = 'inline-block';
     document.getElementById("CustomerF").style.display = 'none';
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
-    document.getElementById("CustomerForm").addEventListener('click', function () {
+    document.getElementById("CustomerPorm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
     document.getElementById("CarF").style.display = 'none';
     document.getElementById("CustomerF").style.display = 'inline-block';
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
-    document.getElementById("DriverForm").addEventListener('click', function () {
+    document.getElementById("DriverPorm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
     document.getElementById("CarF").style.display = 'none';
     document.getElementById("CustomerF").style.display = 'none';
     document.getElementById("DriverF").style.display = 'inline-block';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
-    document.getElementById("CarRentalForm").addEventListener('click', function () {
+    document.getElementById("CarRentalPorm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
     document.getElementById("CarF").style.display = 'none';
     document.getElementById("CustomerF").style.display = 'none';
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'inline-block';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
-    document.getElementById("PaymentForm").addEventListener('click', function () {
+    document.getElementById("PaymentPorm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
     document.getElementById("CarF").style.display = 'none';
     document.getElementById("CustomerF").style.display = 'none';
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'inline-block';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
+});
+    document.getElementById("RentalRequestPorm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'inline-block' ;
+    document.getElementById("IncomeReportF").style.display = 'none';
+});
+    document.getElementById("IncomeReportPorm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'inline-block';
 });
 
